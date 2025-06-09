dad0d14399879d6cd19ddf61c4b256f0faf0e61e
Jigsawsam <bgt117@gmail.com>
2025-06-09 14:54:31 -0700

    ssl

--- database/index.js
+++ database/index.js
@@ -10,9 +10,9 @@ let pool
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    //ssl: {
+      //rejectUnauthorized: false,
+    //},
 })
 
 // Added for troubleshooting queries
@@ -32,9 +32,9 @@ module.exports = {
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    //ssl: {
-      //rejectUnauthorized: false,
-    //},
+    ssl: {
+      rejectUnauthorized: false,
+    },
   })
   module.exports = pool
 }
